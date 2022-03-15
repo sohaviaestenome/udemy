09784ad4a064610bfd37e7c97b9ebfd80063a424
pacs <pacs@outlook.pt>
2022-03-15 20:49:39 +0000

    finished palindrome exercise

--- interview/AlgoCasts/exercises/palindrome/index.js
+++ interview/AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,13 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    const reversed = str.split('').reverse().join('');
+    if (reversed === str) {
+        return true
+    } else {
+        return false;
+    }
+}
 
 module.exports = palindrome;
