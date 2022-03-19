a7e48bc6944f6dabebb35f19eb356762e275dea1
pacs <pacs@outlook.pt>
2022-03-19 18:46:41 +0000

    added hash collision prevention to keys method explanation

--- interview/ztm/tinkers/app.js
+++ interview/ztm/tinkers/app.js
@@ -88,16 +88,27 @@ class HashTable {
       return undefined;
     }
     
-    keys(){
-      const keysArray = [];
-      console.log(this.data.length);
-      for (let i = 0; i < this.data.length; i++){
-        if(this.data[i]){
-          keysArray.push(this.data[i][0][0])
+    keys() {
+        if (!this.data.length) {
+          return undefined
         }
+        let result = []
+        // loop through all the elements
+        for (let i = 0; i < this.data.length; i++) {
+            // if it's not an empty memory cell
+            if (this.data[i] && this.data[i].length) {
+              // but also loop through all the potential collisions
+              if (this.data.length > 1) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                  result.push(this.data[i][j][0])
+                }
+              } else {
+                result.push(this.data[i][0])
+              } 
+            }
+        }
+        return result; 
       }
-      return keysArray;
-    }
   }
   
   const myHashTable = new HashTable(50);
