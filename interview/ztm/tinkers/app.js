bb7789964871a98d655fef460a9b931c8a438ab9
pacs <pacs@outlook.pt>
2022-03-19 18:45:24 +0000

    added keys search method explanation and tinker

--- interview/ztm/tinkers/app.js
+++ interview/ztm/tinkers/app.js
@@ -1,3 +1,6 @@
+
+//----------------------ARRAYS----------------------------------
+
 //exercicio para fazer reverse de string
 const reverse = str => {
     //check input first
@@ -44,3 +47,63 @@ function mergeSortedArrays(array1,array2) {
 }
 
 console.log(mergeSortedArrays([0,6,4,9], [1,99,5,3]));
+
+
+// ----------------------------HASH TABLES-------------------------------------
+
+//Hash table Class
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+      // this.data = [];
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+  
+    set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
+  
+    get(key){
+      const address = this._hash(key);
+      const currentBucket = this.data[address]
+      if (currentBucket) {
+        for(let i = 0; i < currentBucket.length; i++){
+          if(currentBucket[i][0] === key) {
+            return currentBucket[i][1]
+          }
+        }
+      }
+      return undefined;
+    }
+    
+    keys(){
+      const keysArray = [];
+      console.log(this.data.length);
+      for (let i = 0; i < this.data.length; i++){
+        if(this.data[i]){
+          keysArray.push(this.data[i][0][0])
+        }
+      }
+      return keysArray;
+    }
+  }
+  
+  const myHashTable = new HashTable(50);
+  myHashTable.set('grapes', 10000)
+  myHashTable.set('grapes', 10000)
+  myHashTable.get('grapes')
+  myHashTable.set('apples', 9)
+  myHashTable.get('apples')
+  myHashTable.keys()
