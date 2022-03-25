5e18146932c6cc16ddd7cb4d0c877fa7c622a5d9
pacs <pacs@outlook.pt>
2022-03-25 16:21:53 +0000

    adicionei middleware

--- ZTM/nodejs/express/server.js
+++ ZTM/nodejs/express/server.js
@@ -4,8 +4,61 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+    {
+        id:0,
+        name: 'Alber Einstein'
+    },
+    {
+        id:1,
+        name: 'Sir Isaac Newton'
+    }
+];
+
+app.use((req,res,next) => {
+    const start = Date.now();
+    next();
+    //depois de encontrar o route do pedido realizado volta para o middleware para realizar mais acções 
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.post('/friends', (req,res) => {
+    if (!req.body.name) {
+        return res.status(400).json({
+            error: 'Missing friend name'
+        });
+    }
+    const newFriend = {
+        name: req.body.name,
+        id: friends.length
+    };     
+    friends.push(newFriend);
+    
+    res.json(newFriend);
+});
+
 app.get('/', (req,res) => {
     res.send("Hello, man!");
+});
+
+app.get('/friends', (req,res) => {
+    res.json(friends);
+});
+
+app.get('/friends/:friendId', (req,res)=>{
+    const friendId = Number(req.params.friendId);
+    const friend = friends[friendId];
+    if(friend){
+        res.json(friend);
+    } else {
+        res.status(404).json({
+            error: "Friend does not exist"
+        });
+    }
+
 });
 
 app.get('/messages', (req,res) => {
