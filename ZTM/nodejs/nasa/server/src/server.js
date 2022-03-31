7763c8e33cbda56ec1fb7b83931f57b8000c8fff
pacs <pacs@outlook.pt>
2022-03-31 13:06:39 +0100

    added dotenv and tinkered with minor stuff

--- ZTM/nodejs/nasa/server/src/server.js
+++ ZTM/nodejs/nasa/server/src/server.js
@@ -1,33 +1,24 @@
 const http = require('http');
-const mongoose = require('mongoose');
-const app = require('./app');
 
-const { loadPlanetsData } = require('./models/planets.model')
+require('dotenv').config();
+
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
+const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://nasa-api:rFh3LtnIYvkuAvHA@nasaztm.1rybp.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () =>{
-    console.log('MongoDB connection ready');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
-    
-    await loadPlanetsData();
-
-    server.listen(PORT, () => {
-        console.log(`Listening on ${PORT}...`)
-    });
+  await mongoConnect();
+  await loadPlanetsData();
+  await loadLaunchData();
+  
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
 }
 
 startServer();
-
-
