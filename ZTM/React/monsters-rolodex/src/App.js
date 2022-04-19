eedd359d3aa71f7c4ffd3e779189abfd1877ea36
pacs <pacs@outlook.pt>
2022-04-19 13:43:49 +0100

    tudo feito com classes

--- ZTM/React/monsters-rolodex/src/App.js
+++ ZTM/React/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import React,{ Component } from 'react';
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 
 
 class App extends Component {
@@ -8,34 +9,47 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Pedro',
-             lastName: 'Silva'
-            },
-      company: 'Nenhuma'
+      monsters: [],
+      searchField: ''
     }
   }
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then( users => 
+      this.setState(
+        () => {
+          return {monsters: users };
+        }
+      ), 
+      () => {
+        console.log(this.state);
+      }
+      );
+  }
 
+  onSearchChange = event => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState( () => {
+      return { searchField }
+    });
+  }
 
   render(){
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(monster => {
+    return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}</p>
-          <button onClick={ () => {
-            this.setState({name: {firstName: 'Xerife', lastName: 'DaMoita'}
-            })
-           }}> Change Name </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className = 'app-title'>Monsters Rolodex</h1>
+        <SearchBox onChangeHandler = {onSearchChange} placeholder = 'search monsters' className = 'search-box-monsters' />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
     }
